008c624e543262ad2a25d7503690e570677eed7d
Shubham Kumar <shubhamdiscover@gmail.com>
2015-12-12 14:27:21 +0530

    Release 2

--- WebContent/View/script/app.js
+++ WebContent/View/script/app.js
@@ -1,5 +1,5 @@
-﻿var app = angular.module('mainApp', ['ui.bootstrap','ngRoute']);
-app.config(['$routeProvider',function($routeProvider){
+﻿var app = angular.module('mainApp', ['ui.bootstrap']);
+/*app.config(['$routeProvider',function($routeProvider){
 	$routeProvider.when('/biding',{
 	    title:'Map',
 	    templateUrl:'gmap.html',
@@ -9,34 +9,56 @@ app.config(['$routeProvider',function($routeProvider){
 	        templateUrl: 'index.html',
 	        controller: 'mainCtrl'
 		});
-}]);
-app.controller('mainCtrl', ['$scope', '$http','$location' , function ($scope, $http,$location) {
-    console.log('mainctrl');
-    $http.get("api/GetTotalPlace").success(function (response) {
-        $scope.totalPlace = response;
-    });
-    getAllPlaces();
-
-    $http.get("api/AllNameCategory").success(function (response) {
-            $scope.allNameCatgs = response;
-    });
-    $scope.$watch('searchValue',function(){
-    	if($scope.searchValue == "")
-    		getAllPlaces();
-    });
-    $scope.setSearchData = function() {
-    	$http.get("api/PlaceByNameCategory/"+$scope.searchValue).success(function (response) {
-            $scope.places = response;
-        });
-  	};
-  	function getAllPlaces() {
-    	$http.get("api/GetAllPlaces").success(function (response) {
-            $scope.places = response;
-        });
-	};
-  	
-}]);
-
-app.controller('gmapCtrl',function($scope,$rootScope){
-	
+}]);*/
+app.filter('orderByRating', function(){
+	return function(input, attribute) {
+		var array = [];
+		for(var objectKey in input) {
+			array.push(input[objectKey]);
+		}
+		array.sort(function(a, b){
+			a = parseInt(a[attribute]);
+			b = parseInt(b[attribute]);
+			return b - a;
+		});
+		return array;
+	}
 });
+app.controller('mainCtrl', ['$scope', '$http','$location' , function ($scope, $http,$location) {
+	console.log('mainctrl');
+	$http.get("api/GetTotalPlace").success(function (response) {
+		$scope.totalPlace = response;
+	});
+	getAllPlaces();
+
+	$http.get("api/AllNameCategory").success(function (response) {
+		$scope.allNameCatgs = response;
+	});
+	$scope.$watch('searchValue',function(){
+		if($scope.searchValue == "")
+			getAllPlaces();
+	});
+	$scope.setSearchData = function() {
+		$http.get("api/PlaceByNameCategory/"+$scope.searchValue).success(function (response) {
+			$scope.places = response;
+		});
+	};
+	$scope.isChecked = false;
+	$scope.isSort = function(){
+		if(!$scope.isChecked)
+			$scope.sortBy = 'rating';
+		else
+			$scope.sortBy = '';
+		$scope.isChecked = !$scope.isChecked;
+	}
+	function getAllPlaces() {
+		$http.get("api/GetAllPlaces").success(function (response) {
+			$scope.places = response;
+		});
+	};
+
+}]);
+
+/*app.controller('gmapCtrl',function($scope,$rootScope){
+
+});*/
